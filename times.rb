c9b25c9f9918bfea310b4794a2c5cd3600de2cf3
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-08 00:28:33 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,24 @@
+#def using_times
+#  5.times do
+#    puts "Penguins like to jump off icebergs!"
+#  end
+#end
+
+#def using_times
+#  jewels_in_bag = 100
+#  5.times do
+#    puts "Hiding 10 stolen jewels."
+#    jewels_in_bag = jewels_in_bag - 10
+#  end
+#  puts "We have #{jewels_in_bag} jewels still to hide!"
+#end
+
 def using_times
-	#your code here
+  7.times do
+    puts "Wingardium Leviosa"
+  end
 end
 
-
+using_times
 
 
