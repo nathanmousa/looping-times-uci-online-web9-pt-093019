027f93bdeb60f650c56e208f438a6427e37c62d2
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-08 00:26:08 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -5,10 +5,12 @@
 #end
 
 def using_times
+  jewels_in_bag = 100
   5.times do
     puts "Hiding 10 stolen jewels."
     jewels_in_bag = jewels_in_bag - 10
   end
+  puts "We have #{jewels_in_bag} jewels still to hide!"
 end
 
 using_times
