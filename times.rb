4fc8185ad7f2ef65385e09ed2d2c1d2207e89e76
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-08 00:21:41 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,11 @@
+#def using_times
+#  5.times do
+#    puts "Penguins like to jump off icebergs!"
+#  end
+#end
+
 def using_times
-  5.times do
-    puts "Penguins like to jump off icebergs!"
-  end
+  
 end
 
 using_times
